941555a2cb8c42fe8e7969f7739b1cd13355825c
arielle-m <ariem.marii@gmail.com>
2024-06-25 00:16:58 -0700

    added tailwind cdn & custom htmlEl nav & footer

---
+++ js.js
@@ -0,0 +1,70 @@
+class NavBar extends HTMLElement {
+    constructor() {
+        super();
+    }
+
+    connectedCallback() {
+        console.log("NavBar custom element added to page.");
+        this.innerHTML = `
+            <header>
+                <nav>
+                    <ul>
+                        <li><a href="#home">Home</a></li>
+                        <li><a href="#projects">Projects</a></li>
+                        <li><a href="#about">About</a></li>
+                        <li><a href="#contact">Contact</a></li>
+                    </ul>
+                </nav>
+            </header>
+        `;
+    }
+    
+    disconnectedCallback() {
+        console.log("NavBar custom element removed from page.");
+    }
+    
+    adoptedCallback() {
+        console.log("NavBar custom element moved to new page.");
+    }
+    
+    attributeChangedCallback(name, oldValue, newValue) {
+        console.log(`Attribute ${name} has changed on NavBar.`);
+    }
+}
+customElements.define("am-navbar", NavBar);
+
+class Footer extends HTMLElement {
+    constructor() {
+        super();
+    }
+
+    connectedCallback() {
+        console.log("Footer custom element added to page.");
+        this.innerHTML = `
+            <footer>
+                <nav>
+                    <ul>
+                        <li><a href="#home">Email</a></li>
+                        <li><a href="#projects">Linkedin</a></li>
+                        <li><a href="#about">GitHub</a></li>
+                        <li><a href="#contact">Contact</a></li>
+                    </ul>
+                </nav>
+            </footer>
+        `;
+    }
+    
+    disconnectedCallback() {
+        console.log("Footer custom element removed from page.");
+    }
+    
+    adoptedCallback() {
+        console.log("Footer custom element moved to new page.");
+    }
+    
+    attributeChangedCallback(name, oldValue, newValue) {
+        console.log(`Attribute ${name} has changed on Footer.`);
+    }
+}
+customElements.define("am-footer", Footer);
+
