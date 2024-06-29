4f26c4ab0415b1f3ed4671698219eea9460f750e
arielle-m <ariem.marii@gmail.com>
2024-06-28 20:43:20 -0700

    reorganized files and renamed js.js to components.js

--- js/components.js
+++ js/components.js
@@ -8,7 +8,7 @@ class NavBar extends HTMLElement {
         this.innerHTML = `
             <header>
                 <nav>
-                    <ul>
+                    <ul class="flex">
                         <li><a href="#home">Home</a></li>
                         <li><a href="#projects">Projects</a></li>
                         <li><a href="#about">About</a></li>
@@ -43,7 +43,7 @@ class Footer extends HTMLElement {
         this.innerHTML = `
             <footer>
                 <nav>
-                    <ul>
+                    <ul class="flex">
                         <li><a href="#home">Email</a></li>
                         <li><a href="#projects">Linkedin</a></li>
                         <li><a href="#about">GitHub</a></li>
