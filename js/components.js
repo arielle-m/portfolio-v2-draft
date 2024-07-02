e9e353e4494764a8e98167da3d62a9843dadc97f
arielle-m <ariem.marii@gmail.com>
2024-07-01 23:00:35 -0700

    added some styling and the parallax sections; styling with tailwind cdn needs to be embedded though

--- js/components.js
+++ js/components.js
@@ -8,7 +8,7 @@ class NavBar extends HTMLElement {
         this.innerHTML = `
             <header>
                 <nav>
-                    <ul class="flex">
+                    <ul class="flex fixed bottom-0 justify-evenly w-full py-4 bg-neutral-100">
                         <li><a href="#home">Home</a></li>
                         <li><a href="#projects">Projects</a></li>
                         <li><a href="#about">About</a></li>
@@ -42,12 +42,12 @@ class Footer extends HTMLElement {
         console.log("Footer custom element added to page.");
         this.innerHTML = `
             <footer>
+                <a href="#top">Back to top</a>
                 <nav>
                     <ul class="flex">
                         <li><a href="#home">Email</a></li>
                         <li><a href="#projects">Linkedin</a></li>
                         <li><a href="#about">GitHub</a></li>
-                        <li><a href="#contact">Contact</a></li>
                     </ul>
                 </nav>
             </footer>
